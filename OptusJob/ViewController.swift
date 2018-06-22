d26f938dbaf5a5c345cfdb8cfb80f888063c409c
garymansted <unseenfilmz@hotmail.com>
2018-06-22 14:48:45 +1000

    Initial Commit

---
+++ OptusJob/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OptusJob
+//
+//  Created by Gary Mansted on 22/6/18.
+//  Copyright © 2018 Gary Mansted. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
