066d21f252036cb3d3cfda5749fa5ca7d8a3fb8d
Gary Mansted <unseenfilmz@hotmail.com>
2018-07-14 17:47:18 +1000

    Removal of my personal API keys

---
+++ OptusJob/Models/Configs.swift
@@ -0,0 +1,24 @@
+//
+//  Configs.swift
+//  OptusJob
+//
+//  Created by Gary Mansted on 22/6/18.
+//  Copyright © 2018 Gary Mansted. All rights reserved.
+//
+
+import Foundation
+
+let MAIN_APP_COLOR = UIColor(red: 118/255, green: 214/255, blue: 1, alpha: 1)
+let NAV_BAR_BUTTON_COLOR = UIColor.darkGray
+let NAV_BAR_TITLE_COLOR = UIColor.white
+let NAV_BAR_ATTRIBUTES = [NSAttributedStringKey.foregroundColor : UIColor.white,
+                          NSAttributedStringKey.font : UIFont(name: "HelveticaNeue-Light", size: 26)!]
+let WEATHER_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+let GOOGLE_MAPS_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+let SEARCH_BY_ID_URL = URL(string: "http://api.openweathermap.org/data/2.5/weather?id=")!
+let REFRESH_TIME = 60.0
+let ACTIVITY_INDICATOR_SPINNER_COLOR = UIColor.white
+
+
+
+
