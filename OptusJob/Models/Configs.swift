6750906f96b9380d22b553381d8a147493a8984b
Gary Mansted <garymansted@gmail.com>
2021-05-05 19:25:07 +1000

    Project Update

--- OptusJob/Models/Configs.swift
+++ OptusJob/Models/Configs.swift
@@ -13,9 +13,9 @@ let NAV_BAR_BUTTON_COLOR = UIColor.darkGray
 let NAV_BAR_TITLE_COLOR = UIColor.white
 let NAV_BAR_ATTRIBUTES = [NSAttributedString.Key.foregroundColor : UIColor.white,
                           NSAttributedString.Key.font : UIFont(name: "HelveticaNeue-Light", size: 26)!]
-// Enter your own api keys below:
-let WEATHER_API_KEY = "37f08cdac85a5846c278a1ae7cf94e81"
-let GOOGLE_MAPS_API_KEY = "AIzaSyCi3J73QLMztHC31dSfk0OqdQSeOjyVvYs"
+// Enter your own API keys below:
+let WEATHER_API_KEY = SECRET_WEATHER_API_KEY
+let GOOGLE_MAPS_API_KEY = SECRET_GOOGLE_MAPS_API_KEY
 let SEARCH_BY_ID_URL = URL(string: "http://api.openweathermap.org/data/2.5/weather?id=")!
 let REFRESH_TIME = 60.0
 let ACTIVITY_INDICATOR_SPINNER_COLOR = UIColor.white
